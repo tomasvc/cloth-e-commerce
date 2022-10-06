988cd841cd3007201b14c1230a405fdbd58c71e8
Tom <tomas.v0147@gmail.com>
2022-10-06 16:59:55 +0100

    Add service worker fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import * as serviceWorker from "./serviceWorkerRegistration";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import { Provider } from "react-redux";
 import { store, persistor } from "./store";
 import { PersistGate } from 'redux-persist/integration/react'
@@ -22,5 +22,6 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+serviceWorkerRegistration.register();
 reportWebVitals();
-serviceWorker.register();
